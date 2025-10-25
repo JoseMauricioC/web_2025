659f2f8dc0652efb0d3647897612a1a70a1ee5ea
JoseMauricioC <josemauriciocamacho1@gmail.com>
2025-10-25 18:42:25 -0300

    para el CRUD enpesamos con los archivos index y crear, no esta programado en php todavia

---
+++ desarrollo_web/crear.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h2>Crear nuevo Artista</h2>
+        <form action="" enctype="multipart/form-data">
+            <label for="fotografia">Fotografia:</label>
+            <input type="text" name="fotografia"><br><br>
+
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre"><br><br>
+
+            <label for="nacionalidad">Nacionalidad</label>
+            <input type="text" name="nacionlidad"><br><br>
+
+            <input type="submit" value="Guardar">
+        </form>
+
+        <a href="index.php">Volver</a>
+    </div>
+</body>
+</html>
---
+++ desarrollo_web/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <h2>Artistas</h2>
+    <a href="crear.php">Crear</a>
+    <table border='1'>
+        <tr>
+            <th>ID</th>
+            <th>Fotografia</th>
+            <th>Nombre</th>
+            <th>Nacionalidad</th>
+            <th>Acciones</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td> <a href="">Editar</a> <a href="">Eliminar</a></td>
+        </tr>
+
+    </table>
+</div>
+</body>
+</html>
